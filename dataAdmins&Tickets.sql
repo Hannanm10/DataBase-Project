4c0c182168a3c8074fa69b62a05c643855635bf7
Areebaaabid <areebaa.abid01@gmail.com>
2024-05-03 11:43:19 +0500

    Script to add data in database

---
+++ dataAdmins&Tickets.sql
@@ -0,0 +1,7 @@
+
+
+insert into MUser
+values ('Hannan','1234','Admin'),('MatchAdmin','abcd','Matches Manager'),('TicketAdmin','5678','Tickets Manager')
+
+insert into Tickets
+values ('Standard',1000),('FirstClass',1500),('Premium',3000),('VIP',5000),('VVIP',8000)
